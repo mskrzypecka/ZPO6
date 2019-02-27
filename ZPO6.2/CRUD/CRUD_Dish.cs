8c569f419883c82e7f3e30d833a082c77f63fc24
Malgorzata Skrzypecka <malgorzata.skrzypecka@capgemini.com>
2019-02-27 08:12:39 +0100

    CRUD files added

---
+++ ZPO6.2/CRUD/CRUD_Dish.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZPO6._2.CRUD
+{
+    class CRUD_Dish
+    {
+    }
+}
---
+++ ZPO6.2/SqlConnection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZPO6._2
+{
+    public class SqlConnection
+    {
+        string connection;
+
+        public SqlConnection(string connection)
+        {
+            this.connection = connection;
+        }
+
+        public IDataReader ExecuteQuery(string query)
+        {
+            IDataReader reader;
+
+            using (var db = new System.Data.SqlClient.SqlConnection(connection))
+            {
+                db.Open();
+                var command = db.CreateCommand();
+                command.CommandText = query;
+                reader = command.ExecuteReader();
+            }
+            return reader;
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            int result;
+
+            using (var db = new System.Data.SqlClient.SqlConnection(connection))
+            {
+                db.Open();
+                var command = db.CreateCommand();
+                command.CommandText = query;
+                result = command.ExecuteNonQuery();
+            }
+            return result;
+        }
+    }
+}

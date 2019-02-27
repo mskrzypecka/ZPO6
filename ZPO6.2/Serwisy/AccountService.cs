82d692351dc0a55eebf3f377d0e269b0af6f31da
Malgorzata Skrzypecka <malgorzata.skrzypecka@capgemini.com>
2019-02-27 11:34:34 +0100

    AccountService done

--- ZPO6.2/Serwisy/AccountService.cs
+++ ZPO6.2/Serwisy/AccountService.cs
@@ -3,34 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZPO6._2.CRUD;
+using ZPO6._2.Modele;
 
 namespace ZPO6._2
 {
     class AccountService : IAccountService
     {
-        public void ChangeAdress()
+        CRUD_User user;
+        CRUD_Address adres;
+
+        public AccountService(string connection)
         {
-            throw new NotImplementedException();
+            user = new CRUD_User(connection);
+            adres = new CRUD_Address(connection);
         }
 
-        public void ChangePassword()
+        public void ChangeAdress(AdresZamieszkania model) => adres.Update(model);
+
+        public void ChangePassword(Uzytkownik user, string password)
         {
-            throw new NotImplementedException();
+            user.Haslo = password;
+            this.user.Update(user);
         }
 
-        public void CreateUser()
+        public Uzytkownik CreateUser(string login, string password, string firstname, string lastname, string email, string phone, AdresZamieszkania address)
         {
-            throw new NotImplementedException();
+            long id = this.user.GetAll().Count() > 0 
+                        ? this.user.GetAll().Max(x => x.ID) + 1 
+                        : 1;
+
+            var customer = new Uzytkownik
+            {
+                ID = id,
+                Login = login,
+                Haslo = password,
+                Imie = firstname,
+                Nazwisko = lastname,
+                Email = email,
+                NrTelefonu = phone,
+                Adres = address,
+                DataRejestracji = DateTime.Now
+            };
+            this.user.Create(customer);
+
+            return customer;
         }
 
-        public void DeleteUser()
-        {
-            throw new NotImplementedException();
-        }
+        public void DeleteUser(Uzytkownik user) => this.user.Delete(user);
 
-        public void SelectAllUsers()
-        {
-            throw new NotImplementedException();
-        }
+        public List<Uzytkownik> SelectAllUsers() => user.GetAll();
     }
 }
--- ZPO6.2/Serwisy/Interfejsy/IAccountService.cs
+++ ZPO6.2/Serwisy/Interfejsy/IAccountService.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZPO6._2.Modele;
 
 namespace ZPO6._2
 {
     public interface IAccountService
     {
-        void CreateUser();
-        void ChangePassword();
-        void ChangeAdress();
-        void DeleteUser();
-        void SelectAllUsers();
+        Uzytkownik CreateUser(string login, string password, string firstname, string lastname, string email, string phone, AdresZamieszkania address);
+        void ChangePassword(Uzytkownik user, string password);
+        void ChangeAdress(AdresZamieszkania model);
+        void DeleteUser(Uzytkownik user);
+        List<Uzytkownik> SelectAllUsers();
     }
 }
